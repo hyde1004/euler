64c3a0498a178f78bc337f43add0d8c7a29501ce
junggu.lee <junggu.lee@lge.com>
2014-11-25 10:18:35 +0900

    filename change to euler

--- test_euler.py
+++ test_euler.py
@@ -1,10 +1,10 @@
 import unittest
-import solved
+import euler
 
 class TestEuler(unittest.TestCase):
 	def test_problem1(self):
-		self.assertEqual(solved.problem1(10, [3, 5]), 23)
-		self.assertEqual(solved.problem1(16, [3, 5]), 60) # duplicate number
+		self.assertEqual(euler.problem1(10, [3, 5]), 23)
+		self.assertEqual(euler.problem1(16, [3, 5]), 60) # duplicate number
 
 if __name__ == '__main__':
 	unittest.main()
